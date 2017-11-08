b20aae4e7f179c855641524260d2e4c996dd3863
unknown <martin1v93@gmail.com>
2017-11-08 18:51:19 -0500

    Added Friend Circles exercise

---
+++ FriendCircles/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+////https://leetcode.com/problems/friend-circles/description/
+//There are N students in a class. Some of them are friends, while some are not.Their friendship is transitive in nature.For example, if A is a direct friend of B, and B is a direct friend of C, then A is an indirect friend of C.And we defined a friend circle is a group of students who are direct or indirect friends.
+//Given a N*N matrix M representing the friend relationship between students in the class. If M[i][j] = 1, then the ith and jth students are direct friends with each other, otherwise not.And you have to output the total number of friend circles among all the students.
+namespace FriendCircles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] M = { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } };
+            Console.Write(FindCircleNum(M));
+        }
+
+        public static int FindCircleNum(int[,] M)
+        {
+            int result = 0;
+            bool[] visited = new bool[M.GetLength(0)];
+
+            for (int i = 0; i < M.GetLength(0); i++)
+                if (!visited[i])
+                {
+                    Console.WriteLine("Friend circle " + i + ": ");
+                    Dfs(i, M, visited);
+                    result++;
+                }
+
+            return result;
+        }
+
+        private static void Dfs(int startNode, int[,] M, bool[] visited)
+        {
+            visited[startNode] = true;
+
+            for (int i = 0; i <= M.GetLength(1) - 1; i++)
+            {
+                if (startNode == i)
+                    continue;
+
+                if (M[startNode, i] == 1 && !visited[i])
+                {
+                    Dfs(i, M, visited);
+                    Console.WriteLine(startNode + " is friends with " + i);
+                }
+            }
+        }
+    }
+}
---
+++ ScrambledImages/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ScrambledImages
+{
+    class Program
+    {
+        private static readonly Color Red = Color.FromArgb(255, 0, 0);
+        static void Main(string[] args)
+        {
+            var baseMap = new Bitmap(args[0]);
+            var resultMap = CreateNewMap(baseMap);
+            using (var stream = File.Create(Path.Combine(Path.GetDirectoryName(args[0]), "Result.png")))
+            {
+                resultMap.Save(stream, ImageFormat.Bmp);
+            }
+        }
+
+        private static Bitmap CreateNewMap(Bitmap baseMap)
+        {
+            var resultMap = new Bitmap(baseMap.Width, baseMap.Height);
+            for (int x = 0; x < baseMap.Width; x++)
+            {
+                var rowList = new List<Color>();
+                for (int y = 0; y < baseMap.Height; y++)
+                {
+                    var pixel = baseMap.GetPixel(y, x);
+                    rowList.Add(pixel);
+                }
+                var colors = GenerateRow(rowList);
+                for (int i = 0; i < colors.Count; i++)
+                {
+                    resultMap.SetPixel(i, x, colors[i]);
+                }
+            }
+            return resultMap;
+        }
+
+        public static List<Color> GenerateRow(List<Color> input)
+        {
+            var count = input.Count(x => x == Red);
+            var index = input.IndexOf(Red);
+            var list = new List<Color>();
+            var startRange = input.GetRange(index + count, (input.Count - index) - count);
+            var endRange = input.GetRange(0, index + count);
+            list.AddRange(startRange);
+            list.AddRange(endRange);
+            return list;
+        }
+    }
+}
